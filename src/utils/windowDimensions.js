12a0cd2c51880e1479cf1f04f5b47b976cde3753
Robyn Bursnell <robyn.bursnell@asda.uk>
2022-04-04 11:33:14 +0100

    Increased equality in hoping the website will build.

--- src/utils/windowDimensions.js
+++ src/utils/windowDimensions.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 
 const getWindowDimensions = () => {
-  if (typeof window != undefined) {
+  if (typeof window !== undefined) {
     const { innerWidth: width, innerHeight: height } = window
     return {
       width,
